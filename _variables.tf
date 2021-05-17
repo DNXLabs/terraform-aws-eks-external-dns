11c01cf75cab06886a407dbe343096cc187fe74a
Arthur Diniz <arthurbdiniz@gmail.com>
2021-05-17 16:58:49 -0300

    Upgrade chart to v5.0.0
    Signed-off-by: Arthur Diniz <arthurbdiniz@gmail.com>

--- _variables.tf
+++ _variables.tf
@@ -33,7 +33,7 @@ variable "helm_chart_name" {
 
 variable "helm_chart_version" {
   type        = string
-  default     = "3.4.1"
+  default     = "5.0.0"
   description = "Version of the Helm chart."
 }
 
